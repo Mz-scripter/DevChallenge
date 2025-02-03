fa59bb522012e92731b865b759a846fa12193b4f
Mz-scripter <mzscripterx5@gmail.com>
2025-02-03 23:02:24 +0100

    Added advanced gaming functionality.

--- test.py
+++ test.py
@@ -1,17 +1,13 @@
 import os
 import random
+from DevChallenge.settings import BASE_DIR
 
-ICONS_FOLDER = './icons'
+ICONS_FOLDER = os.path.join(BASE_DIR, "static/icons")
 
 def get_random_icon():
-    """Fetch a random programming language icon."""
     icons = [f for f in os.listdir(ICONS_FOLDER) if f.endswith(".svg")]
     if icons:
         return random.choice(icons)
     return None
 
-logo = get_random_icon()
-logo_name = logo.split("-")[0]
-
-print(logo)
-print(logo_name)
+print(get_random_icon()) # Output: None
