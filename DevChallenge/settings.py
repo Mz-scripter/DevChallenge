8480499b4ecfda024c42a6421b83ed931836392b
Mz-scripter <mzscripterx5@gmail.com>
2025-02-08 21:44:27 +0100

    Added the leaderboard.

--- DevChallenge/settings.py
+++ DevChallenge/settings.py
@@ -125,3 +125,6 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+SESSION_COOKIE_AGE = 86400
+SESSION_SAVE_EVERY_REQUEST = True
