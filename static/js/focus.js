aae9f173e4dbbc8453784289ada40482a180140f
Mz-scripter <mzscripterx5@gmail.com>
2025-02-23 23:11:48 +0100

    Fix: Delay Focus and simulate user interaction.

--- static/js/focus.js
+++ static/js/focus.js
@@ -1,1 +1,13 @@
-document.getElementById("answer").focus();
+function focusInput() {
+    const input = document.getElementById("answer");
+
+    if (input) {
+        input.setAttribute("readonly", "readonly");
+        input.setAttribute("disabled", "true");
+        input.focus();
+        input.removeAttribute("readonly");
+        input.removeAttribute("disabled");
+    }
+}
+
+setTimeout(focusInput, 100);
