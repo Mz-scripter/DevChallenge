b0772ad7946b71ebe71655d8b6c2f12a15d77d79
Adeko Muheez <161574712+Mz-scripter@users.noreply.github.com>
2025-02-23 19:54:44 +0100

    Update share-stats.js

--- static/js/share-stats.js
+++ static/js/share-stats.js
@@ -10,7 +10,7 @@ function shareStats() {
         link.download = "game-stats.png";
         link.click();
     });
-    const link = "github.com";
+    const link = "https://cooperative-legra-mz-scripter-2b0021a5.koyeb.app/";
     let text = encodeURIComponent(`Hey dev! 👋 Just played #DevChallenge! Can you guess dev icons faster than me? Check out my stats and give it a shot here: ${link}`);
     let twitterUrl = `https://twitter.com/intent/tweet?text=${text}`;
     window.open(twitterUrl, "_blank");
